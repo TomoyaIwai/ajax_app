6812571abb15f89e498a5d014d1397af5760e0f5
TomoyaIwai <tomoyanbaby@gmail.com>
2022-03-22 17:42:34 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root  'users#index'
   get 'posts', to: 'posts#index'
   get 'posts/new', to: 'posts#new'
   post 'posts', to: 'posts#create'
+  root to: 'posts#index'
 end
